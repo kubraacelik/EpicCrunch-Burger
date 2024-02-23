49de060edf69ffb6edd9ae245820f9adf58f2d78
kubraacelik <kkbra.celik92@gmail.com>
2024-02-24 00:13:41 +0300

    added navbar and footer components

---
+++ burger/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Her girdiğim sayfada navbar ve footer'ın olmasını istedğim için buraya ekledim */}
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
+        <Route path="/menu" exact element={<Menu />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
